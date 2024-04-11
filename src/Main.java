73b6707f93db43c50622bf319db42b72c3453ca3
dollemika <dollemika@mail.ru>
2024-04-11 11:20:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("как тебя зовут?");
+		String name;
+		Scanner read = new Scanner(System.in);
+		name = read.next();
+		if 		(name.charAt(name.length()-1) == 'а' ||
+				name.charAt(name.length()-1) == 'я' ||
+				name.charAt(name.length()-1) == 'и' ||
+				name.charAt(name.length()-1) == 'А' ||
+				name.charAt(name.length()-1) == 'И' ||
+				name.charAt(name.length()-1) == 'Я')
+			System.out.println("Приветик!");
+		else
+			System.out.println("Здарова!");
+	}
+}
